d249a3de5c95a9a2d4b0ec0a0f592ad2190f21df
kahsi13 <134238928+kahsi13@users.noreply.github.com>
2025-03-29 21:47:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,27 +3,13 @@ from pydantic import BaseModel
 from transformers import AutoTokenizer
 import onnxruntime
 import numpy as np
-import requests
 
 app = FastAPI()
 
-# Hugging Face model bilgileri
-hf_model_id = "Kahsi13/DomatesRailway"
-onnx_url = f"https://huggingface.co/{hf_model_id}/resolve/main/bert_domates_model.onnx"
+# Tokenizer ve ONNX modeli yükle
+tokenizer = AutoTokenizer.from_pretrained("Kahsi13/DomatesRailway")
+session = onnxruntime.InferenceSession("bert_domates_model_quant.onnx")  # 👈 quant dosyası
 
-# ONNX model dosyasını indir (sadece ilk çalıştırmada)
-onnx_path = "bert_domates_model.onnx"
-if not os.path.exists(onnx_path):
-    print("🔽 Hugging Face'den model indiriliyor...")
-    r = requests.get(onnx_url)
-    with open(onnx_path, "wb") as f:
-        f.write(r.content)
-
-# Tokenizer ve ONNX oturumu
-tokenizer = AutoTokenizer.from_pretrained(hf_model_id)
-session = onnxruntime.InferenceSession(onnx_path)
-
-# Giriş modeli
 class InputText(BaseModel):
     text: str
 
